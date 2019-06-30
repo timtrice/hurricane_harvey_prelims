9bca6e292baa1ab03b03cbb8e0bcd38f2b12edf0
Tim Trice <tim.trice@gmail.com>
2019-06-30 11:10:15 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GH_REPO="@github.com/$TRAVIS_REPO_SLUG.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git config --global user.name "Travis CI"
+git config --global user.email "tim.trice@gmail.com"
+git clone https://github.com/timtrice/hurricane_harvey_prelims.git
+cd distill
+git checkout develop
+Rscript -e 'rmarkdown::render_site(".");'
+
+if [ ! -d "docs" ]
+then
+  echo "Docs directory does not exist"
+  exit 1
+fi
+
+git add --force docs
+MSG="Rebuild website, $(date) [skip ci]"
+git commit -m "$MSG"
+git push --force $FULL_REPO develop
+Rscript -e 'remotes::install_github("karthik/holepunch");'
+Rscript -e 'holepunch::build_binder();'
